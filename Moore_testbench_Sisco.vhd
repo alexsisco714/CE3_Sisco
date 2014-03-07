0a3e78978d20418e2eb4d08223c377d41932fc11
Alex Sisco <C16alex.sisco@usafa.edu>
2014-03-06 20:53:00 -0700

    Moore Finished

---
+++ Moore_testbench_Sisco.vhd
@@ -0,0 +1,114 @@
+--------------------------------------------------------------------------------
+-- Company: USAFA/DFEC
+-- Engineer: Alex Sisco
+--
+-- Create Date:   19:08:26 03/06/2014
+-- Design Name:   Elevator Controller
+-- Module Name:   C:/Users/C16Alex.Sisco/Documents/ISE Projects/CE3_Sisco/Moore_testbench_Sisco.vhd
+-- Project Name:  CE3_Sisco
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: MooreElevatorController_Shell
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY Moore_testbench_Sisco IS
+END Moore_testbench_Sisco;
+ 
+ARCHITECTURE behavior OF Moore_testbench_Sisco IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT MooreElevatorController_Shell
+    PORT(
+         clk : IN  std_logic;
+         reset : IN  std_logic;
+         stop : IN  std_logic;
+         up_down : IN  std_logic;
+         floor : OUT  std_logic_vector(3 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '0';
+   signal stop : std_logic := '0';
+   signal up_down : std_logic := '0';
+
+ 	--Outputs
+   signal floor : std_logic_vector(3 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: MooreElevatorController_Shell PORT MAP (
+          clk => clk,
+          reset => reset,
+          stop => stop,
+          up_down => up_down,
+          floor => floor
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+		reset<='1';
+      wait for 100 ns;	
+		reset<='0';
+      wait for clk_period*10;
+		
+		--going up
+		up_down<='1';
+		wait for clk_period;
+		stop<='1';
+		wait for clk_period*2;
+		stop<='0';
+		wait for clk_period;
+		stop<='1';
+		wait for clk_period*2;
+		stop<='0';
+		wait for clk_period;
+		stop<='1';
+		wait for clk_period*2;
+		stop<='0';
+		--back down
+		up_down<='0';
+      wait;
+   end process;
+
+END;
